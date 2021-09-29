36e79f42a01c7dc83db860eba3809173ce8a04c0
cbobed <cbobed@unizar>
2021-09-29 08:26:10 +0200

    Added a first version of the naive merge + post-acceptance pruning

--- sid/PartMining/CodeTable.py
+++ sid/PartMining/CodeTable.py
@@ -184,7 +184,7 @@ def calculate_complete_size (codetable, standard_codetable):
         if codetable[label]['usage'] != 0:
             codetable_size += -math.log(codetable[label]['usage'] / sum_usage_codetable)
             for item in codetable[label]['code']:
-                codetable_size += sct_codelengths[item]
+                codetable_size += sct_codelengths[int(item)]
     print(f'database_size: {database_size}')
     print(f'codetable_size: {codetable_size}')
     print(f'complete: {database_size+codetable_size}')
@@ -244,60 +244,50 @@ def merge_codetables_naive(codetables):
 ## we merge the tables and we apply the KRIMP pruning after the merging to
 ## avoid possible local decisions that might hid different codes
 ## PRE: codetables with the support and the usages calculated
+
+# we adapt the post-acceptance pruning to the case where we have different
+# source tables, w don't care about the original lengths, but about the original
+# code usages (sum of all the usages in the original tables for the repeated
+# codes)
+
 def merge_codetables_pruning(codetables, database):
     merged = {}
     non_colision_labelbase = 0
-    sum_usage = 0
+
     for ct in codetables:
-        sum_usage = 0
         for label in ct:
             current_label = str(non_colision_labelbase) + '_' + str(label)
             merged[current_label] = ct[label]
-            sum_usage += ct[label]['usage']
-        # we extend each code with their local codelength
-        for label in ct:
-            if ct[label]['usage'] > 0:
-                ct[label]['code_length']= -math.log(ct[label]['usage'] / sum_usage)
-            else:
-                ct[label]['code_length'] = 0
         non_colision_labelbase += 1
 
     # we keep track of the codes that are duplicated
     to_omit = set()
+    merged_groups = {}
     merged_key_list = list(merged.keys())
     for i in range(len(merged_key_list)):
         if merged_key_list[i] not in to_omit:
+            current_label = merged_key_list[i]
+            merged_groups[current_label] = [merged_key_list[i]]
             for j in range(i + 1, len(merged_key_list)):
                 if merged_key_list[j] not in to_omit:
                     # depending on how the table has been obtained it might or not have the code_set field
                     ## converted using the analysis table (convert_int_codetable) => it has
                     ## loaded from file => it hasn't
-                    if 'code_set' in merged[merged_key_list[i]]:
-                        set_i = merged[merged_key_list[i]]['code_set']
+                    if 'code_set' in merged[current_label]:
+                        set_i = merged[current_label]['code_set']
                     else:
-                        set_i = set([int(item) for item in merged[merged_key_list[i]]['code']])
+                        set_i = set([int(item) for item in merged[current_label]['code']])
                     if 'code_set' in merged[merged_key_list[j]]:
                         set_j = merged[merged_key_list[j]]['code_set']
                     else:
                         set_j = set([int(item) for item in merged[merged_key_list[j]]['code']])
-                    # if we have to omit jth element, we add its usage and support
-                    # to do the microaverage
+
                     if set_i == set_j:
-                        i_table, i_code = merged_key_list[i].split('_')
-                        j_table, j_code = merged_key_list[j].split('_')
-
-                        # We could use the mean of the usage proportions as well, reflexion on that is needed
-                        if 'code_lengths_array' not in merged[merged_key_list[i]]:
-                            merged[merged_key_list[i]]['code_lengths_array'] = [codetables[int(i_table)][int(i_code)]['code_length']]
-                            merged[merged_key_list[i]]['dbs'] = 1
-
-                        merged[merged_key_list[i]]['code_lengths_array'].append(codetables[int(j_table)][int(j_code)]['code_length'])
-                        merged[merged_key_list[i]]['dbs'] += 1
-
+                        # mark the duplicated label to get rid of, but add it to the associated
+                        # group
                         to_omit.add(merged_key_list[j])
+                        merged_groups[current_label].append(merged_key_list[j])
 
-    # we get rid of the duplicated entries in the codetable
-    [merged.pop(code) for code in to_omit]
 
     new_merged = copy.deepcopy(merged)
     for label in new_merged:
@@ -305,81 +295,105 @@ def merge_codetables_pruning(codetables, database):
             new_merged[label]['code'] = [str(item) for item in new_merged[label]['code_int']]
         new_merged[label]['usage'] = 0
         new_merged[label]['support'] = 0
-        new_merged[label]['code_length'] = 0
-        if 'dbs' in new_merged[label]:
-            new_merged[label].pop('code_lengths_array')
-            new_merged[label].pop('dbs')
 
+    # we get rid of the duplicated entries in the codetable
+    [new_merged.pop(code) for code in to_omit]
+    # I copy this version as my clean candidate codetable
+    new_merged_candidate = copy.deepcopy(new_merged)
 
+    for label in merged_groups:
+        for entry in merged_groups[label]:
+            new_merged[label]['usage'] += merged[entry]['usage']
+            new_merged[label]['support'] += merged[entry]['support']
 
     ## To prune according to KRIMP, we need the previous supports and usages
+    calculate_codetable_support(database, new_merged_candidate)
+    converted_new_merged_candidate_sco = codetable_in_standard_cover_order(new_merged_candidate)
+    calculate_codetable_usage(database, converted_new_merged_candidate_sco)
 
-    calculate_codetable_support(database, new_merged)
-    print(f'{new_merged.keys()}')
-    converted_new_merged_sco = codetable_in_standard_cover_order(new_merged)
-    print(f'{converted_new_merged_sco.keys()}')
-    calculate_codetable_usage(database, converted_new_merged_sco)
-
-    sum_usage = 0
-    for label in converted_new_merged_sco:
-        sum_usage += converted_new_merged_sco[label]['usage']
-    for label in converted_new_merged_sco:
-        if converted_new_merged_sco[label]['usage'] > 0:
-            converted_new_merged_sco[label]['code_length'] = -math.log(converted_new_merged_sco[label]['usage'] / sum_usage)
-        else:
-            converted_new_merged_sco[label]['code_length'] = 0
-
-    # let's check how many have changed their code_lengths
-    increased_length = 0
-    equal_length = 0
-    decreased_length = 0
+    # let's check how many have changed their usages
+    increased_usage = 0
+    equal_usage = 0
+    decreased_usage = 0
     not_used_before = 0
     not_used_after = 0
 
     stored_info = {}
-    for label in converted_new_merged_sco:
-        prev_label = converted_new_merged_sco[label]['old_label']
-        if 'dbs' in merged[prev_label]:
-            print(f'{[x for x in merged[prev_label]["code_lengths_array"] if x != 0]}')
-            if len([x for x in merged[prev_label]['code_lengths_array'] if x != 0]) != 0:
-                code_length_before = statistics.mean([x for x in merged[prev_label]['code_lengths_array'] if x != 0])
+    for label in converted_new_merged_candidate_sco:
+        prev_label = converted_new_merged_candidate_sco[label]['old_label']
+        code_usage_before = new_merged[prev_label]['usage']
+        if converted_new_merged_candidate_sco[label]['usage'] != 0 and code_usage_before != 0:
+            if converted_new_merged_candidate_sco[label]['usage'] > code_usage_before:
+                increased_usage += 1
+                stored_info[label] = {'diff': converted_new_merged_candidate_sco[label]['usage'] - code_usage_before}
+            elif converted_new_merged_candidate_sco[label]['usage']< code_usage_before:
+                decreased_usage += 1
             else:
-                code_length_before = 0
+                equal_usage += 1
+
         else:
-            code_length_before = merged[prev_label]['code_length']
-        if converted_new_merged_sco[label]['code_length'] != 0 and code_length_before != 0:
-            if converted_new_merged_sco[label]['code_length'] > code_length_before:
-                increased_length += 1
-                stored_info[label] = {'diff': converted_new_merged_sco[label]['code_length']-code_length_before}
-            elif converted_new_merged_sco[label]['code_length']< code_length_before:
-                decreased_length += 1
-            else:
-                equal_length += 1
-        else:
-            if converted_new_merged_sco[label]['code_length'] == 0:
+            if converted_new_merged_candidate_sco[label]['usage'] == 0:
                 not_used_after +=1
-            if code_length_before == 0:
+            if code_usage_before == 0:
                 not_used_before += 1
 
-    print(f'increased_length :: {increased_length}')
-    print(f'equal_length :: {equal_length}')
-    print(f'decreased_length ::{decreased_length }')
+    print(f'increased_usage :: {increased_usage}')
+    print(f'equal_usage :: {equal_usage}')
+    print(f'decreased_usage ::{decreased_usage }')
     print(f'not_used_before :: {not_used_before}')
     print(f'not_used_after :: {not_used_after}')
-    print(f'stored_info ::{[(x,stored_info[x]["diff"]) for x in sorted(stored_info.keys(), key=lambda x: stored_info[x]["diff"], reverse=True)]}')
+    # print(f'stored_info ::{[(x,stored_info[x]["diff"]) for x in sorted(stored_info.keys(), key=lambda x: stored_info[x]["diff"], reverse=True)]}')
     ## We leave a flag to avoid duplicated calculations (to be cleaned ... sometime, somewhere ...)
 
-    print(converted_new_merged_sco.keys())
-    print(f'before pruning: {calculate_size_database_from_codetable(converted_new_merged_sco)}')
-    calculate_codetable_support(database, converted_new_merged_sco)
-    new_new = codetable_in_standard_cover_order(converted_new_merged_sco)
-    calculate_codetable_usage(database, new_new)
-    print(f'after pruning: {calculate_size_database_from_codetable(new_new)}')
+    print(f'starting the pruning ... ')
+    # we have new_merged with the previous information merged
+    # we have converted_new_merged_candidate_sco with the information adapted to the global database
+    # we have also the code labels (of new_merged_candidate) of the codes that have decreased their usage
+    ##      According to KRIMP's algorithm: those are the starting prune candidate set
 
+    #In the original algorithm, it test all the codes
+    # PruneCand ← X ∈ PruneSet    with lowest usageCTc (X)
+    # we skip the singletons, just to be sure
+
+    database_sct = build_SCT(database)
+    print(f'number of codes before pruning: {len(converted_new_merged_candidate_sco)}')
+    print(f'database size before: {calculate_size_database_from_codetable(converted_new_merged_candidate_sco)}')
+    prune_set = set(label for label in stored_info if len(converted_new_merged_candidate_sco[label]['code']) > 1)
+    print(f'prune_candidate_set: {len(prune_set)}')
+    while len(prune_set) != 0:
+        code_candidate = sorted([label for label in prune_set], key=lambda x: len(converted_new_merged_candidate_sco[x]['code']), reverse=True)[0]
+        prune_set.remove(code_candidate)
+        print(f'checking: {converted_new_merged_candidate_sco[code_candidate]["code"]}')
+        print(f'prune_candidate_set: {len(prune_set)}')
+        aux_pruned = copy.deepcopy(converted_new_merged_candidate_sco)
+        aux_pruned.pop(code_candidate)
+
+        # we wouldn't have to update the supports, as the D hasn't change,
+        # we have only to update the usages, but there might be a problem with the support
+        # of the singletons => TODO
+        for aux_label in aux_pruned:
+            aux_pruned[aux_label]['support'] = 0
+            aux_pruned[aux_label]['usage'] = 0
+
+        calculate_codetable_support(database, aux_pruned)
+        converted_new_merged_pruned_sco = codetable_in_standard_cover_order(aux_pruned)
+        calculate_codetable_usage(database, converted_new_merged_pruned_sco)
+
+        current_size = calculate_complete_size(converted_new_merged_candidate_sco, database_sct)
+        pruned_size = calculate_complete_size(converted_new_merged_pruned_sco, database_sct)
+
+        if pruned_size < current_size:
+            for label in converted_new_merged_pruned_sco:
+                if len(converted_new_merged_pruned_sco[label]['code']) > 1:
+                    if (converted_new_merged_pruned_sco[label]['usage']<converted_new_merged_candidate_sco[label]['usage']):
+                        prune_set.add(label)
+            converted_new_merged_candidate_sco = converted_new_merged_pruned_sco
+            print(f'code pruned __ before: {current_size} -> after: {pruned_size}')
+    print(f'number of codes after pruning: {len(converted_new_merged_candidate_sco)}')
+    print(f'database size after: {calculate_size_database_from_codetable(converted_new_merged_candidate_sco)}')
     # we have all the previous information about the codes in merged and the new in new_merged, we have to
     # prune new_merged according to KRIMP prune step
-
-    return converted_new_merged_sco
+    return converted_new_merged_candidate_sco
 
 if __name__ == "__main__":
     my_parser = argparse.ArgumentParser(allow_abbrev=False)
@@ -416,20 +430,20 @@ if __name__ == "__main__":
         aux_codetable = read_codetable(current_name + '.ct', True)
         aux_converted_codetable = convert_int_codetable(aux_codetable, aux_db_dat_table)
 
-        if (args.merge_method == 'pruning'):
-            # we need to calculate the local supports and usages
-            aux_dat_database = tdb.read_database_dat(current_name + '.dat')
-
-            calculate_codetable_support(aux_dat_database, aux_converted_codetable)
-            print(f'num codes: {len(aux_codetable)}')
-            print(f'codes with support 0: {[x for x in aux_converted_codetable if aux_converted_codetable[x]["support"] == 0]}')
-            aux_codetable_sco = codetable_in_standard_cover_order(aux_converted_codetable)
-            calculate_codetable_usage(aux_dat_database, aux_codetable_sco)
-            print(f'codes with usage 0: {[x for x in aux_codetable_sco if aux_codetable_sco[x]["usage"] == 0]}')
-
-            codetables.append(aux_codetable_sco)
-        else:
-            codetables.append(aux_converted_codetable)
+        # if (args.merge_method == 'pruning'):
+        #     # we need to calculate the local supports and usages
+        #     aux_dat_database = tdb.read_database_dat(current_name + '.dat')
+        #
+        #     calculate_codetable_support(aux_dat_database, aux_converted_codetable)
+        #     print(f'num codes: {len(aux_codetable)}')
+        #     print(f'codes with support 0: {[x for x in aux_converted_codetable if aux_converted_codetable[x]["support"] == 0]}')
+        #     aux_codetable_sco = codetable_in_standard_cover_order(aux_converted_codetable)
+        #     calculate_codetable_usage(aux_dat_database, aux_codetable_sco)
+        #     print(f'codes with usage 0: {[x for x in aux_codetable_sco if aux_codetable_sco[x]["usage"] == 0]}')
+        #
+        #     codetables.append(aux_codetable_sco)
+        # else:
+        codetables.append(aux_converted_codetable)
 
     dat_database = tdb.read_database_dat(args.database_file)
     print(f'number of codetables: {len(codetables)}')
