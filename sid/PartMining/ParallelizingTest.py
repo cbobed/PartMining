63c34892c09961e8d4579da147d525ce8dccbff3
cbobed <cbobed@unizar>
2021-10-22 10:41:22 +0200

    Changed from apply to starmap functions: apply DOES not offer concurrency, while starmap DOES

--- sid/PartMining/ParallelizingTest.py
+++ sid/PartMining/ParallelizingTest.py
@@ -12,6 +12,7 @@ import multiprocessing as mp
 import time
 import psutil as ps
 
+
 results = []
 
 def function_to_map(list_to_process):
@@ -20,9 +21,16 @@ def function_to_map(list_to_process):
         local_dict[x] +=1
     return local_dict
 
+def function_to_apply (list_to_process, shared_dict):
+    local_dict = {x:0 for x in range (100)}
+    for x in list_to_process:
+        local_dict[x] += 1
+    for label in local_dict:
+        shared_dict[label] += local_dict[label]
+
 if __name__ == '__main__':
     print('initializing ... ')
-    list = [[random.randint(0,99) for x in range(2000000)] for y in range (100)]
+    list = [[random.randint(0,99) for x in range(200000)] for y in range (100)]
 
     print ('non-parallelized ... ')
     start = time.time()
@@ -43,5 +51,20 @@ if __name__ == '__main__':
             dict_parallel[y] += dict_result[y]
     end = time.time()
     print(f'parallelized with {ps.cpu_count(logical=False)}: {end-start}')
+    pool.close()
 
+    print(f'second parallelilzation - shared memory ')
+    start = time.time()
+    with mp.Manager() as manager:
+        shared_dict = manager.dict()
+        for x in range(100):
+            shared_dict[x] = 0
+        pool = mp.Pool(ps.cpu_count(logical=False))
+        [pool.starmap(function_to_apply, [(l,shared_dict) for l in list])]
+        end = time.time();
+        print(f'parallelilzed using shared memory : {end-start}')
+        pool.close()
+        print(f'everything among parallelized ok: {all([dict_parallel[x] == shared_dict[x] for x in dict_parallel])}')
+        print(shared_dict)
+        print(dict_parallel)
     print(f'everything ok: {all([dict_parallel[x] == dict[x] for x in dict_parallel])}')
