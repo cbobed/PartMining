1b6c512582ceb3aaff254817f4dfa49f2cf2d3b8
cbobed <cbobed@unizar>
2022-03-18 09:51:01 +0100

    added a utility calculator script for the item and trans size of a dataset

--- sid/PartMining/DatabaseItemTransSizeCalculator.py
+++ sid/PartMining/DatabaseItemTransSizeCalculator.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     args=my_parser.parse_args()
     database = tdb.read_database_dat(args.database_file)
     with open(args.database_file+".transSize", "w") as output:
-        output.write(len(database))
+        output.write(str(len(database)))
 
     num_items = 0
     for t in database:
         num_items += len(t)
     with open(args.database_file+".itemSize", "w") as output:
-        output.write(num_items)
+        output.write(str(num_items))
