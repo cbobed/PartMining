a67e8475386270d47e9cb906b5e0ece8767da02c
cbobed <cbobed@unizar.es>
2023-07-25 17:04:03 +0200

    cleaned

--- experimentalResults/mergeComparison/mergesEnd/curateMergedInfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-for f in `find ./ -name '*.out'`; do 
-	grep 'merged table size without non-used codes:\|merged ratio:\|-- MERGING' $f > ${f%.*}.curated
-	grep 'merge accepted' $f | wc -l >> ${f%.*}.curated
-	grep 'merge rejected' $f | wc -l >> ${f%.*}.curated
-done
