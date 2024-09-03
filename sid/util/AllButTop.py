17626d3286632ec026f96e0a86a9f596c7dffd97
cbobed <cbobed@unizar.es>
2024-09-03 08:41:18 +0200

    Added a method to nullify all but the main PCA directions

--- sid/util/AllButTop.py
+++ sid/util/AllButTop.py
@@ -20,4 +20,21 @@ def all_but_the_top(v, D):
 	u = PCA(n_components=D).fit(v_tilde).components_  # [D, emb_size]
 	# Subtract first `D` principal components
 	# [vocab_size, emb_size] @ [emb_size, D] @ [D, emb_size] -> [vocab_size, emb_size]
-	return v_tilde - (v @ u.T @ u)  
+	return v_tilde - (v @ u.T @ u)
+
+
+def top_but_the_all(v, D):
+	"""
+	Arguments:
+	:v: word vectors of shape (n_words, n_dimensions)
+	:D: number of principal components to subtract
+	"""
+	# 1. Subtract mean vector
+	v_tilde = v - np.mean(v, axis=0)
+	# 2. Compute the first `D` principal components
+	#    on centered embedding vectors
+	u = PCA().fit(v_tilde).components_  # [emb_size, emb_size]
+	u_rest = u[D:,]
+	# Subtract first `D` principal components
+	# [vocab_size, emb_size] @ [emb_size, emb_size-D] @ [emb_size-D, emb_size] -> [vocab_size, emb_size]
+	return v_tilde - (v @ u_rest.T @ u_rest)
